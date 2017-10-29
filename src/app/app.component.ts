23ffdbed350c6e1e2f3b531b2f404da9019e209c
elteKrisztianKereszti <krisztian.kereszti@gmail.com>
2017-10-29 19:21:11 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+const RockStyles: string[] = [
+  "Hard Rock",
+  "Punk",
+  'Heavy Metal'
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  rockStyles = RockStyles;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GLinkComponent } from './glink.component';
+import { MyComponent } from './my.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GLinkComponent,
+    MyComponent
   ],
   imports: [
     BrowserModule
